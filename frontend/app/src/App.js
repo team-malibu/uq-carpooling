ba3c307d533e22190f1519814e506900be176823
Andrew Tauber <andrewtauber@Andrews-MacBook-Pro.local>
2021-08-19 16:48:42 +1000

    Branched from master

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import BlankDefaultPage from "./components/BlankDefaultPage";
 function App() {
   return (
-    <div className="App">
-      <h1>Header</h1>
-    </div>
+    <>
+      <Router>
+        <Switch>
+        <Route path='/' exact component={BlankDefaultPage} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
