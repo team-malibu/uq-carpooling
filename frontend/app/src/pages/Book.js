31e73ed2b24cf36affad452600d3efbf8153850f
Conal (Scooby) O'Leary <olearyconal01@gmail.com>
2021-09-01 10:31:46 +1000

    Basic use case routing

--- frontend/app/src/pages/Book.js
+++ frontend/app/src/pages/Book.js
@@ -3,15 +3,27 @@ import BlankDefaultPage from '../components/BlankDefaultPage'
 import Navbar from '../components/Navbar'
 import TripMap from '../components/TripMap'
 import TripTile from '../components/TripTile'
+import {useHistory} from 'react-router-dom'
+import { SquareButton } from '../components/Button'
+import './Book.css'
 
 function Book() {
-    return (
-        <div>
-           <BlankDefaultPage currentlySelected={0} name='Book' />
-            <TripMap/>
-        </div>
-        
-    )
+  const history = useHistory();
+
+  return (
+    <div>
+      <BlankDefaultPage currentlySelected={0} name='Book' />
+      <div class='bbtile'>
+        <TripTile />
+      </div>
+      <TripMap />
+      <div class='bbbutton' onClick={() => history.push('/select')}>
+        <SquareButton/>
+      </div>
+
+    </div>
+
+  )
 }
 
 export default Book
